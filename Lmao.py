ea7d28964c73e99d926919c68c307f949fb21546
TfdoIcare <166625568+TfdoIcare@users.noreply.github.com>
2024-04-10 09:59:46 -0400

    Add files via upload

---
+++ Lmao.py
@@ -0,0 +1,11 @@
+def encoder(stri):
+    l=[]
+    nstr=''
+    for num in stri:
+        l.append(num)
+    for num in l:
+        nstr=nstr+str(int(num)+3)
+    return nstr
+
+a=encoder('000')
+print(a)
